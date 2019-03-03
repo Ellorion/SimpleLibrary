7ce9ba5f8a4c530ed3907e639eec01d20d4f600d
Ellorion <ellorion@live.de>
2019-03-03 14:12:10 +0100

    updated basic_event_handler example

--- examples/basic_event_handler.cpp
+++ examples/basic_event_handler.cpp
@@ -2,9 +2,22 @@
 #include "test/core.h"
 
 instant void
-Window_HandleEvents(Window *window) {
+Window_HandleEvents(
+	Window *window
+) {
 	MSG msg;
 	bool running = true;
+
+	ShaderSet  shader_set = ShaderSet_Create(window);
+	Keyboard  *keyboard   = window->keyboard;
+	Font       font       = Font_Load(S("default.ttf"), 16);
+
+	Assert(keyboard);
+
+	if (Font_HasError(&font)) {
+		MessageBox(window->hWnd, font.s_error.value, 0, MB_OK);
+		return;
+	}
 
 	while(running) {
 		msg = {};
@@ -14,12 +27,16 @@ Window_HandleEvents(Window *window) {
 		Window_ReadMessage(msg, running, window, false);
 		OpenGL_AdjustScaleViewport(window, false);
 
+		if (keyboard->up[VK_ESCAPE])
+			running = false;
+
 		/// Render
 		/// ===================================================================
 		OpenGL_ClearScreen();
-
 		Window_UpdateAndResetInput(window);
 	}
+
+	ShaderSet_Destroy(&shader_set);
 }
 
 int main() {
@@ -28,7 +45,7 @@ int main() {
 	Keyboard keyboard;
 	Mouse    mouse;
 
-	Window_Create(&window, "Hello, World!", 800, 480, 32, &keyboard, &mouse);
+	Window_Create(&window, "Demo-Application", 800, 480, 32, &keyboard, &mouse);
 	Window_Show(&window);
 
 	OpenGL_Init(&window);
