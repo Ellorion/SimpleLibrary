740708252e1e99f218a0d918c4a74d4c7eb59da5
Ellorion <ellorion@live.de>
2019-03-31 12:16:46 +0200

    refined ticket mutex

--- src/core/mutex.h
+++ src/core/mutex.h
@@ -6,22 +6,47 @@ struct Mutex_Ticket {
 };
 
 instant void
-Mutex_Begin(
+Mutex_Lock(
 	Mutex_Ticket *mutex
 ) {
 	Assert(mutex);
 
 	s64 ticket = InterlockedIncrement(&mutex->ticket);
 
-	while(ticket != mutex->serving)
+	while(ticket > mutex->serving)
 		Sleep(1);
 }
 
 instant void
-Mutex_End(
+Mutex_LockUntil(
+	Mutex_Ticket *mutex,
+	s64 serving
+) {
+	Assert(mutex);
+
+	while(serving != mutex->serving)
+		Sleep(1);
+}
+
+instant void
+Mutex_Release(
 	Mutex_Ticket *mutex
 ) {
 	Assert(mutex);
 
-	mutex->serving = InterlockedIncrement(&mutex->serving);
+	if (mutex->serving < mutex->ticket)
+		mutex->serving = InterlockedIncrement(&mutex->serving);
+	else
+		LOG_WARNING("Mutex_Release before Mutex_Lock.");
 }
+
+instant void
+Mutex_Reset(
+	Mutex_Ticket *mutex
+) {
+	Assert(mutex);
+
+	InterlockedExchange(&mutex->ticket , 0);
+	InterlockedExchange(&mutex->serving, 0);
+}
+
