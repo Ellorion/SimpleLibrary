82d60273df621d911a4287d45640547047ddbae3
Ellorion <ellorion@live.de>
2019-04-16 18:55:58 +0200

    updated font_inspector for easier folder change

--- examples/font_inspector.cpp
+++ examples/font_inspector.cpp
@@ -36,11 +36,16 @@ int main() {
 	Font 		font_static 	= Font_Load(S("default.ttf"), 20);
 	Font 		font_dynamic 	= Font_Load(S("default.ttf"), 20);
 
-	Widget wg_label 		= Widget_CreateLabel(  &window, &font_static , {0, 0, 250                    , 0}, S("Dir: ./fonts"));
+	String s_font_directory = S("./fonts");
+
+	Widget wg_label 		= Widget_CreateLabel(  &window, &font_static , {0, 0, 250                    , 0}, S("Dir: "));
 	Widget wg_list  		= Widget_CreateListBox(&window, &font_static , {0, 0, wg_label.rect_content.w, 0});
 	Widget wg_font_plus 	= Widget_CreateButton( &window, &font_static , {}, S("+"));
 	Widget wg_font_minus 	= Widget_CreateButton( &window, &font_static , {}, S("-"));
 	Widget wg_textbox 		= Widget_CreateTextBox(&window, &font_dynamic, {}, true);
+
+	String *s_data_label = Widget_GetTextData(&wg_label);
+	String_Append(s_data_label, s_font_directory);
 
 	Text_UseWordWrap(&wg_textbox.text, true);
 
@@ -76,13 +81,16 @@ int main() {
 	MemorySegment_AddWidgets(&window.a_segments_reset, &ap_widgets);
 
 	Array<Directory_Entry> a_font_files;
-	File_ReadDirectory(&a_font_files, S("./fonts"), DIR_LIST_ONLY_FILES, true, "ttf");
+	File_ReadDirectory(&a_font_files, s_font_directory, DIR_LIST_ONLY_FILES, true, "ttf");
 
 	FOR_ARRAY(a_font_files, it) {
 		Directory_Entry *dir = &ARRAY_IT(a_font_files, it);
 
 		String s_font_file = S(dir->s_name);
-		String_AddOffset(&s_font_file, 8);
+		String_AddOffset(&s_font_file, s_font_directory.length);
+
+		if (String_StartWith(&s_font_file, S("\\"), true))
+			String_AddOffset(&s_font_file, 1);
 
 		Widget_AddRow(&wg_list, S(s_font_file, s_font_file.length -4));
 	}
