f7794e41e117a59b0eb11be92ac30c561800abf5
Ellorion <ellorion@live.de>
2019-08-12 23:35:52 +0200

    removed file dependency from csv data handling

---
+++ src/utility/csv.h
@@ -0,0 +1,24 @@
+#pragma once
+
+instant Array<Array<String>>
+CSV_Load(
+	String s_data
+) {
+	Array<Array<String>> a_csv;
+	Array<String>        as_rowitem;
+
+	if (!String_IsEmpty(&s_data)) {
+		Array<String> as_rows = Array_SplitLinesRef(&s_data, false);
+
+		FOR_ARRAY(as_rows, it_row) {
+			String *s_row = &ARRAY_IT(as_rows, it_row);
+
+			/// can get overwritten after the prev. split was copied
+			as_rowitem = Array_Split(s_row, S(","), DELIMITER_IGNORE, true);
+			/// will copy the content, so do not destroy it
+			Array_Add(&a_csv, as_rowitem);
+		}
+	}
+
+	return a_csv;
+}
