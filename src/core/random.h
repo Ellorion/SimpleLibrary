68e36a46ff924c3512df33667fb110bea143a423
Ellorion <ellorion@live.de>
2021-09-28 23:57:50 +0200

    updated random generator

--- src/core/random.h
+++ src/core/random.h
@@ -1,24 +1,14 @@
 #pragma once
 
-instant void
-Random_Init(
-	u16 seed = 0
-) {
-	if (seed)
-		srand(seed);
-	else
-		srand(time(0));
-}
+// xorshift64s
+instant u64
+Random_Get() {
+    static u64 seed = time(0);
 
-instant s32
-Random_Get(
-	s32 min,
-	s32 max
-) {
-	if (min > max)
-		SWAP(s32, &min, &max);
-
-	s32 value = min + (rand() % (max - min));
-
-	return value;
+    auto x = seed;
+	x ^= x >> 12; // a
+	x ^= x << 25; // b
+	x ^= x >> 27; // c
+	seed = x;
+	return x * 0x2545F4914F6CDD1DULL;
 }
