1036afa8b224523b780b5e492fa665079f739853
Ellorion <ellorion@live.de>
2021-12-16 04:34:24 +0100

    improved imput and no crash on pressing alt-key anymore

--- src/windows/input.h
+++ src/windows/input.h
@@ -2,15 +2,16 @@
 
 #pragma once
 
-#define INPUT_KEY_COUNT 0xFF
+#define INPUT_KEY_COUNT 0xFFFF
 
-/// --- Work in progress
-/// - crashes with alt gr
-/// - some keys do not work like [
+/// Notes:
+/// no accents support
 
 struct InputState {
+    bool key_repeating[INPUT_KEY_COUNT];
     bool key_pressing[INPUT_KEY_COUNT];
     u8 keys_pressed = 0;
+    u8 last_key_repeated = 0;
     u8 last_key_pressed = 0;
 };
 
@@ -20,32 +21,43 @@ Input_UpdateKeyboard(InputState &state) {
     GetKeyboardState(key_states);
 
     state.keys_pressed = 0;
-    state.last_key_pressed = 0;
+    state.last_key_pressed  = 0;
+    state.last_key_repeated = 0;
 
-    FOR(INPUT_KEY_COUNT, it) {
-        auto vkey = GetAsyncKeyState(it);
-        wchar_t ch = 0;
+    /// virtual keys
+    FOR(0xFF, it) {
+        wchar_t wch[5];
 
         s32 result = ToUnicode(
                         it,
-                        MapVirtualKey(it, 0),
-                        key_states, &ch, 1, 0
+                        MapVirtualKey(it, MAPVK_VK_TO_VSC),
+                        key_states, wch, 4, 0
                      );
 
         if (result > 0) {
-            /// pressing (ignoring repeat)
-            bool is_pressing = (vkey & 0x8000);
+            auto vkey = GetAsyncKeyState(it);
 
-            state.key_pressing[ch] = is_pressing;
-            state.keys_pressed += is_pressing;
+            /// pressing (ignoring repeat)
+            bool is_repeating = (vkey & 0x8000);
+            bool is_pressing  = (vkey & 0x0001);
+
+            auto ch = wch[result-1];
+            state.key_pressing[ch]  = is_pressing;
+            state.key_repeating[ch] = is_repeating;
+            state.keys_pressed += is_repeating;
 
             if (is_pressing) {
-                state.last_key_pressed = ch;
+                state.last_key_pressed  = ch;
+            }
+
+            if (is_repeating) {
+                state.last_key_repeated = ch;
             }
         }
     }
 
     if (state.keys_pressed > 1) {
         state.last_key_pressed = 0;
+        state.last_key_repeated = 0;
     }
 }
